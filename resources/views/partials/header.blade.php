acd240db08874b36daf4f508b8b87c530b9df3a6
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-10-22 16:35:03 +0200

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+@php
+ 
+@endphp
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                HEADER SECTION
+            </div>
+        </div>
+    </div>
+</header>
